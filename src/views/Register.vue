9e92a63c73e760960c1b9e7e20f1b94abf0de33f
self-use <gdfvsgfd8769@gmail.com>
2022-01-26 06:52:20 +0800

    1-3 OTP驗證

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -18,13 +18,17 @@
                     </label>
                     <div class="d-flex m-auto justify-content-around">
                         <button class="btn bg-danger text-white"
-                                >回上一頁</button>
-                        <button class="btn bg-primary text-white" @click="formSubmit">送出</button>
+                                @click="this.$router.go(-1)">回上一頁</button>
+                        <button v-if="isLog === false"
+                                class="btn bg-primary text-white" @click="formSubmit">送出</button>
+                        <button v-if="isLog === true"
+                                class="btn bg-primary text-white" @click="OTP">重新驗證</button>
                     </div>
                 </section>
             </div>
         </div>
     </div>
+    <p class="text-white">OTPcode:{{OTPcode}}</p>
     <div v-if="lightbox === true"
          @click="lightbox = false"
          class="gray-back position-fixed top-0 start-0">
@@ -33,9 +37,9 @@
         <img class="close position-absolute top-0 end-0" alt=""
              src="../assets/images/lightbox/close.png"
              @click="lightbox = false">
-        <h2 class="text-center">請完成驗證</h2>
+        <h2 class="text-center">{{OTPcode !== '' ? '請在 30 秒內完成驗證' : '驗證碼失效，請重新驗證'}}</h2>
         <button class="btn rounded bg-primary text-white"
-                @click="verify()">驗證</button>
+                @click="verify()">{{OTPcode !== '' ? '驗證' : '重新驗證'}}</button>
       </div>
     </div>
 </template>
@@ -48,15 +52,15 @@ export default {
   // components: {
   //   lightbox,
   // },
-  emits: [
-    'accepted',
-  ],
   data() {
     return {
       email: '',
       name: '',
       password: '',
       lightbox: false,
+      OTPlink: '',
+      OTPcode: '',
+      isLog: false,
     };
   },
   methods: {
@@ -70,17 +74,37 @@ export default {
           alert('此帳號已被註冊');
           return;
         }
-        this.lightbox = true;
-        this.registed();
-        // alert('註冊成功，進一步完成驗證');
       }
+      this.registed();
+      // alert('註冊成功，進一步完成驗證');
     },
     registed() {
       const created = new Date().getTime();
       this.$emit('emit-reg', this.email, this.name, this.password, created);
+      this.isLog = true;
+      this.OTP();
     },
     verify() {
-      this.$emit('emit-ver');
+      if (this.OTPlink === this.OTPcode) {
+        this.$emit('emit-ver');
+        alert('完成驗證');
+        this.$router.push({ name: 'Home' });
+      }
+    },
+    OTP() {
+      this.lightbox = true;
+      if (this.OTPcode !== '') {
+        this.OTPcode = '';
+      }
+      const seed = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'm', 'n', 'p', 'Q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '2', '3', '4', '5', '6', '7', '8', '9'];
+      for (let i = 0; i < 10; i += 1) {
+        this.OTPcode += seed[Math.floor(Math.random() * seed.length)];
+      }
+      this.OTPlink = this.OTPcode;
+      const OTPlive = setTimeout(() => {
+        this.OTPcode = '';
+      }, 3000);
+      OTPlive();
     },
   },
   created() {
